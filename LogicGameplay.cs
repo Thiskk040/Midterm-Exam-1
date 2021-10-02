a9b6f1c9a98cf89e3fba15008e5fa5c45d69c6e8
Thiskk040 <78652197+Thiskk040@users.noreply.github.com>
2021-10-03 02:45:27 +0700

    Add files via upload
    คุณานนต์ เงาเบญจกุล 63120501040

---
+++ LogicGameplay.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace _50
+{
+   
+   class LogicGameplay
+    {
+        public void Gameplay()//สุ่มคำ และสร้าง ____ คำใบ้...
+        {
+            string[] words = new string[3];
+            words[0] = "Tennis";
+            words[1] = "Football";
+            words[2] = "Badminton";
+
+            Random random = new Random();///ใช้ random ที่พี่ให้มา
+            int resultRandom = random.Next(0,2);
+            string secretword = words[resultRandom];
+
+            char[] inputt = new char[secretword.Length];///สร้าง Array _____
+
+
+            for (int i = 0; i < secretword.Length; i++)
+            {
+                inputt[i] = '_';
+            }
+            counting(secretword, inputt);
+        }
+
+        public void counting(string secretword,char[]inputt)/// เมดทอดนับคำผิดและแสดงผล
+        {
+            int misscount = 0;
+            int rightcount = 0;
+
+
+            bool IsWon = false;/// เงื่อนไขที่ตั้งว่าชนะเป็น false เพื่อที่จะเอาไว้ใส่ตอนชนะจะได้ ใส่เป็น trueเพื่อบอกว่ามึงชนะแล้ว
+
+            do
+            {
+                Console.WriteLine("Incorrect Score : {0}", misscount);///นับคำผิด
+                for (int i = 0; i < secretword.Length; i++)
+                {
+                    Console.Write(inputt[i]);
+                }
+                Console.Write("Input letter alphabet : ");////ใส่ตัวอักษร
+
+                char playerGuess = char.Parse(Console.ReadLine());
+
+                if (secretword.Contains(playerGuess))//// การตรวจสอบคำไปเรื่อบๆ
+                {
+                    for (int j = 0; j < secretword.Length; j++)
+                    {
+                        if (playerGuess == secretword[j])
+                        {
+                            inputt[j] = playerGuess;
+                            rightcount++;
+                        }
+                    }
+
+                    if (rightcount == secretword.Length) ////ถ้าเดาถูกก็จะไม่นับ misscount
+                    {
+                        IsWon = true; 
+                    }
+                }
+                else
+                {
+                    misscount += 1; ///// เดาคำผิดก็ Misscount + 1 ไปดิ
+                }
+            }
+            while (misscount <= 7 && !IsWon);
+
+            if (IsWon = true)/// นี่ไงที่บอกว่าชนะแล้วจะเป็น true 
+            {
+                Console.WriteLine(inputt);
+                Console.WriteLine("You win!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("You loose!!!!!");///// นี่ไงถ้านอกเหนือจากนี้ก็แพ้เพราะว่าหลุด กรณ๊ Iswon = true
+            }
+            Console.ReadLine();
+
+
+        }
+
+    }
+ }
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _50
+{
+    enum HanngmanMenu
+    {
+        playgame=1,
+            exit=2,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printmenuscreen();//แสดงผลลัพธ์หน้าจอที่ main
+        }
+        static void Printmenuscreen()//เมดทอดแสดงผลลัพธ์ main
+        {
+            Console.WriteLine("Welcome to Hangman game");
+            Console.WriteLine();
+            Console.WriteLine("1.Playgame");
+            Console.WriteLine("2.Exit");
+            InputFormTheKeyboard();
+        }
+        static void InputFormTheKeyboard()//รับค่าจากคียบอรด์ โดยใช้ enum
+        {
+            Console.Write("Please sclected menu: ");
+            HanngmanMenu menu = (HanngmanMenu)(int.Parse(Console.ReadLine()));
+            Persentmenu(menu);
+
+        }
+        static void Persentmenu(HanngmanMenu menu)///ฟังก๋ชั่นคำสั่งเมนูหลัก
+        {
+            if (menu == HanngmanMenu.playgame)
+            {
+                Startgame();
+            }
+            else if (menu == HanngmanMenu.exit)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Error Please try again");
+            }
+        }
+        static void Startgame()//เมดทอดเริ่มเกมและแสดงหน้าจอ interface 
+        {
+            Console.WriteLine("Play Hangman Game");
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            LogicGameplay Hang1 = new LogicGameplay();
+            Hang1.Gameplay();
+            
+        }
+       
+
+
+    }
+}
+
